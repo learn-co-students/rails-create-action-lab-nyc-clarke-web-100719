6b7a9a475d7b3bdc2d00b14fea2b71f4e479d896
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-05 16:17:59 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,39 @@
 class StudentsController < ApplicationController
-  
+  before_action :set_student, only: :show
+
   def index
     @students = Student.all
+    render :index
   end
 
   def show
-    @student = Student.find(params[:id])
+    render :show
   end
 
   def new
+    render :new
   end
 
   def create
+    @student = Student.create(student_params)
+
+    if @student.valid?
+      redirect_to student_path(@student) # "/attendees/#{@attendee.id}"
+    else
+      flash[:messages] = @student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  private
+
+  def student_params
+    # params.require(:student).permit(:first_name, :last_name)
+    params.permit(:first_name, :last_name)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 
 end
